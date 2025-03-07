4068526be3ba43a3bcd5eb48b2b26a6191213c71
akina0302 <ymu.m24@gmail.com>
2025-03-07 22:56:02 +0900

    active_atorageの導入

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ApplicationRecord
   belongs_to :category
   has_many :recipe_materials
   has_many :materials , through: :recipe_materials
+  has_one_attached :image
   
   validates :name , presence: true
   
