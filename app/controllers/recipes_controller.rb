857339c541cef9e6225d51f6dc0a77d174c60855
akina0302 <ymu.m24@gmail.com>
2025-03-17 14:58:53 +0900

    編集機能

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,8 @@
 class RecipesController < ApplicationController
 
+  before_action :set_recipe, only: [:show, :edit, :update]
+
+
   def index
     @recipe = Recipe.where(user_id: current_user.id).all
   end
@@ -19,13 +22,36 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find_by(id: params[:id] , user_id: current_user.id)
+  end
+
+  def edit
+    recipe_attributes = @recipe.attributes
+    @recipe_category_material = RecipeCategoryMaterial.new(recipe_attributes)
+  end
+
+  def update
+    @recipe_category_material = RecipeCategoryMaterial.new(recipe_params)
+
+    #画像を変更しない場合は既存の画像をセットする
+    @recipe_category_material.image ||= @recipe.image if @recipe.image.attached?
+
+    if @recipe_category_material.valid?
+      @recipe_category_material.update(@recipe.id)
+      redirect_to root_path
+    else
+      render :edit,status: :unprocessable_entity
+    end
+
   end
     
   private 
 
   def recipe_params
     params.require(:recipe_category_material).permit(:name, :procedure, :url, :category_name, :material_name, :image).merge(user_id: current_user.id)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find_by(id: params[:id] , user_id: current_user.id)
   end
 
 end
